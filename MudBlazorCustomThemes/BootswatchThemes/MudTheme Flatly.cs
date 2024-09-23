6a5b0a6a21cc1c430b31d09349a853b76c6b3da3
Sergiusz Kriwobokow <writesd@hotmail.com>
2024-09-23 14:46:12 +0200

    no comments

---
+++ MudBlazorCustomThemes/BootswatchThemes/MudTheme Flatly.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MudBlazor;
+
+namespace MudBlazorCustomThemes.BootswatchThemes
+{
+    public class ThemeFlatly
+    {
+        MudTheme theme;
+        public MudTheme Get => theme;
+        public ThemeFlatly()
+        {
+            {
+                theme = new MudTheme()
+                {
+                    PaletteLight = new PaletteLight()
+                    {
+                        Primary = Colors.Blue.Default,
+                        Secondary = Colors.Green.Accent4,
+                        AppbarBackground = Colors.BlueGray.Default
+                    },
+                    PaletteDark = new PaletteDark()
+                    {
+                        Primary = Colors.Blue.Lighten1,
+                        AppbarBackground = Colors.BlueGray.Darken3
+
+                    },
+
+                    LayoutProperties = new LayoutProperties()
+                    {
+                        DrawerWidthLeft = "260px",
+                        DrawerWidthRight = "300px"
+                    }
+                };
+            }
+        }
+    }
+}
---
+++ MudBlazorCustomThemes/MyCustomThemes/MyCustomTheme2.cs
@@ -0,0 +1,47 @@
+﻿using MudBlazor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MudblazorCustomThemes.MyCustomThemes
+{
+    public class MyCustomTheme2
+    {
+        MudTheme theme;
+        public MyCustomTheme2()
+        {
+            {
+                theme = new MudTheme()
+                {
+                    PaletteLight = new PaletteLight()
+                    {
+                        Primary = Colors.Blue.Default,
+                        Secondary = Colors.Green.Accent4,
+                        AppbarBackground = Colors.Red.Default,
+                    },
+                    PaletteDark = new PaletteDark()
+                    {
+                        Primary = Colors.Blue.Lighten1
+                    },
+
+                    LayoutProperties = new LayoutProperties()
+                    {
+                        DrawerWidthLeft = "260px",
+                        DrawerWidthRight = "300px"
+                    }
+                };
+            }
+        }
+        public MudTheme Get
+        {
+            get
+            {
+                return theme;
+            }
+        }
+
+    }
+
+}
